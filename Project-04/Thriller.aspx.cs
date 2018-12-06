476cd5d98281069de8b971f340cb6da7c4df0351
Signe Frederiksen <signe_frederiksen@hotmail.dk>
2018-12-06 14:39:41 +0100

    Pagination now works on the rest Animation, sciencefiction and thriller page (still needs to do it on action page).

--- Project-04/Thriller.aspx.cs
+++ Project-04/Thriller.aspx.cs
@@ -12,17 +12,39 @@ namespace Project_04
 {
     public partial class Thriller : System.Web.UI.Page
     {
+        public int CurrentPage
+        {
+            get
+            {
+                // look for current page in ViewState
+                object o = this.ViewState["_CurrentPage"];
+                if (o == null)
+                    return 0; // default page index of 0
+                else
+                    return (int)o;
+            }
+
+            set
+            {
+                this.ViewState["_CurrentPage"] = value;
+            }
+        }
+
+
         // Connection to Database with SQL Selection
         // SIGNES DB
-        // SqlConnection conn = new SqlConnection(@"data source = DESKTOP-VKU3EK5; integrated security = true; database = MovieDB");
+        SqlConnection conn = new SqlConnection(@"data source = DESKTOP-VKU3EK5; integrated security = true; database = MovieDB");
         // AMANDAS DB
-        SqlConnection conn = new SqlConnection(@"data source = LAPTOP-7ILGU10M; integrated security = true; database = MovieDB");
-        SqlCommand cmd = null;
-        SqlDataReader rdr = null;
+        //SqlConnection conn = new SqlConnection(@"data source = LAPTOP-7ILGU10M; integrated security = true; database = MovieDB");
+        SqlDataAdapter da = null;
+        DataSet ds = null;
+        DataTable dt = null;
         string sqlsel = "SELECT * From Movies Left Join Genre On Movies.GenreID = Genre.ID WHERE Genre LIKE '%thriller%' ORDER BY Title asc";
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Call the ShowMovieData method to populate control.
             // Use method for showing the Movie data from Database
             ShowMovieData();
         }
@@ -30,25 +52,55 @@ namespace Project_04
         // Create method for our Movie data
         public void ShowMovieData()
         {
-            try
-            {
-                conn.Open();
+            da = new SqlDataAdapter();
+            da.SelectCommand = new SqlCommand(sqlsel, conn);
 
-                cmd = new SqlCommand(sqlsel, conn);
+            ds = new DataSet();
+            da.Fill(ds, "MovieList");
+            dt = ds.Tables["MovieList"];
 
-                rdr = cmd.ExecuteReader();
-                RepeaterThriller.DataSource = rdr;
-                RepeaterThriller.DataBind();
-            }
-            catch (Exception ex)
-            {
-                //  LabelMessage.Text = ex.Message;
-            }
-            finally
-            {
-                conn.Close();
-            }
+            // Populate the repeater control with the Items DataSet
+            PagedDataSource pds = new PagedDataSource();
+            pds.DataSource = ds.Tables[0].DefaultView;
 
+            // Indicate that the data should be paged
+            pds.AllowPaging = true;
+
+            // Set the number of items you wish to display per page
+            pds.PageSize = 30;
+
+            // Set the PagedDataSource's current page
+            pds.CurrentPageIndex = CurrentPage;
+
+            LabelCurrentPage.Text = (CurrentPage + 1).ToString() + " of "
+            + pds.PageCount.ToString();
+
+            // Disable Prev or Next buttons if necessary
+            ButtonPrev.Enabled = !pds.IsFirstPage;
+            ButtonNext.Enabled = !pds.IsLastPage;
+
+            // Binding the correct data from the PagedDataSource to the Repeater
+            RepeaterThriller.DataSource = pds;
+            RepeaterThriller.DataBind();
+        }
+
+        protected void ButtonPrev_Click(object sender, EventArgs e)
+        {
+            // Set viewstate variable to the previous page
+            CurrentPage -= 1;
+
+            // Reload control
+            ShowMovieData();
+        }
+
+
+        protected void ButtonNext_Click(object sender, EventArgs e)
+        {
+            // Set viewstate variable to the next page
+            CurrentPage += 1;
+
+            // Reload control
+            ShowMovieData();
         }
     }
 }
