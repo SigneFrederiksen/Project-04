5a9eb9d6010dc893c406d70294e43cee426224b6
Signe Frederiksen <signe_frederiksen@hotmail.dk>
2018-12-12 12:33:02 +0100

    Added a-tag to search page. Now it's shows and links to the right movies.

--- Project-04/SingleMovie.aspx.cs
+++ Project-04/SingleMovie.aspx.cs
@@ -18,9 +18,9 @@ namespace Project_04
     {  
         
         // SIGNES DB
-        //SqlConnection conn = new SqlConnection(@"data source = DESKTOP-VKU3EK5; integrated security = true; database = MovieDB");
+        SqlConnection conn = new SqlConnection(@"data source = DESKTOP-VKU3EK5; integrated security = true; database = MovieDB");
         // AMANDAS DB
-        SqlConnection conn = new SqlConnection(@"data source = LAPTOP-7ILGU10M; integrated security = true; database = MovieDB");
+        //SqlConnection conn = new SqlConnection(@"data source = LAPTOP-7ILGU10M; integrated security = true; database = MovieDB");
         SqlCommand cmd = null;
 
 
